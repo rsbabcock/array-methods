f91026754bd238934af5affe0b85cf2579fd0c4d
rsbabcock <nss2@NSS2ss-MacBook-Pro.local>
2018-04-25 10:24:19 -0500

    Added dealership app and some of steves code the for dealership, also added chaining methods back

---
+++ dealership-app.js
@@ -0,0 +1,12 @@
+
+// Copy the JSON from cars.json and assign it to a variable in a new application. 
+// This data holds sales information for a car dealership. 
+// Your job is to produce the following reports for the dealership based on their total 2017 sales.
+
+// Total profit for 2017
+// In which month did they sell the most cars?
+// Which salesperson sold the most cars?
+// Which salesperson made the most profit?
+// Which model was the most popular?
+// Which bank provided the most loans to our customers?
+
